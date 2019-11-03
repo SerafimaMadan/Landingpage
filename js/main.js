67e5225299d7ea8fcd461e86fa98f02001e4da26
Serafima <svp1982@mail.ru>
2019-11-03 15:25:51 +0100

    Clickable menu

--- js/main.js
+++ js/main.js
@@ -18,3 +18,27 @@ $(document).ready(function () {
         $(".modal").hide()
     })
 });
+const positions = [], //сюда сложим на загрузке страницы позиции наших "якорных" блоков, чтобы не считать их каждый раз. и сюда же положим ссылки на соответствующие a.scroll-to
+    currentActive = null, //здесь будет храниться id текущего блока, чтобы не менять классы по 100 раз за одну прокрутку 
+    links = $('.nav-item, .nav-modal'); //сохраним массив всех a.nav-item
+
+$(".anchor").each(function(){ //перебираем блоки, сохраняем позиции и ссылки на пункты меню
+    positions.push({
+        top: $(this).position().top - 100,
+        a: links.filter('[href="#'+$(this).attr('id')+'"]')
+    });
+});
+
+$(window).on('scroll',function() {
+    const winTop = $(window).scrollTop();
+    for(const i = 0; i < positions.length; i++){
+        if(positions[i].top < winTop){ //если прокрутка страницы ниже нашего блока
+            if(currentActive !== i){ //и если мы еще не добавили класс текущему блоку
+                currentActive = i;
+                links.filter('.active').removeClass('active'); //снимаем класс .active с текущего пункта меню
+                positions[i].a.addClass("active");
+            }
+            break; //выходим из цикла, не нужно проверять блоки, которые выше
+        }
+    }
+});
